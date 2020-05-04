c11361dfa38f94a3c3c7e798803953ba0f9ec70b
David Fontenot <font21@gmail.com>
2020-05-04 17:06:24 -0500

    Shuffling function works, render function works

--- app/app.js
+++ app/app.js
@@ -3,24 +3,52 @@ console.log('JavaScript connected.')
 // Setup the groups of card parts and shove them into an aray
 	// Source: https://en.wikipedia.org/wiki/Standard_52-card_deck
 	const suitsArray = ['&diams;', '&hearts;', '&spades;', '&clubs;'];
+
 	const rankArray = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 	const cardDeckArray = [];
 
 // Function to create an new array with cards as objects
-	function deckGenerator() {
-		for ( let i = 0; i < rankArray.length; i++ ) {
-			for (let x = 0;  x < suitsArray.length; x++) {
-				let cardObj = {Value: suitsArray[x], Suit: rankArray[i]};
-				cardDeckArray.push(cardObj);			
+	deckGenerator = () => {
+		for ( let iti = 0; iti < rankArray.length; iti++ ) {
+			for (let ity = 0;  ity < suitsArray.length; ity++) {
+				let cardObj = {Value: suitsArray[ity], Suit: rankArray[iti]};
+				cardDeckArray.push(cardObj);
 			}
 		}
 		return cardDeckArray;
 	}
 
+
+
+// Function to render the card deck
+	renderDeck = () => {
+
+		document.getElementById('deck').innerHTML = '';
+		for (let itw = 0; itw < cardDeckArray.length; itw++) {
+
+			let card = document.createElement("div");
+			let value = document.createElement("div");
+			let suit = document.createElement('div');
+
+			card.className = "card";
+			value.className = "value";
+			suit.className = 'suit' + deck[itw].suit;
+
+			value.innerHTML = cardDeckArray[itw].Value;
+			card.appendChild(value);
+			card.appendChild(suit);
+
+			document.getElementById("deck").appendChild(card);
+		}
+	}
+
+
+
 // Function to shuffle a bazillion times.
-	function shuffleCards() {
-		for (let i = 0; i < 999; i++) {
+	shuffleCards = () => {
+		// Work through cardDeckArray grabbing two random cards and swapping them for 999.
+		for (let itz = 0; itz < 1000; itz++) {
 			let CardPositionOne = Math.floor((Math.random() * cardDeckArray.length));
 			let CardPositionTwo = Math.floor((Math.random() * cardDeckArray.length));
 			
@@ -29,6 +57,23 @@ console.log('JavaScript connected.')
 			cardDeckArray[CardPositionOne] = cardDeckArray[CardPositionTwo];
 			cardDeckArray[CardPositionTwo] = CardPositionThree;
 		}
-
-
+		/* renderDeck(); */
 	}
+
+// Do things Functions
+	function PageloadFunction() {
+		deck = deckGenerator();
+		shuffleCards();
+		renderDeck();
+		console.log(cardDeckArray);
+	}
+
+
+	function buttonPressFunction() {
+		shuffleCards();
+		renderDeck();
+		console.log(cardDeckArray);
+	}
+
+
+	window.onload = PageloadFunction;
